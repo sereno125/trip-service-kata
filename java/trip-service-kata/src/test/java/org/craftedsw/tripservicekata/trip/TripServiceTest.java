635fdb7a17d820dda857846e2e91f309eb3322d5
sereno125 <sereno125@gmail.com>
2017-07-01 10:24:54 +0900

    test coding ....

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,24 +1,61 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
+import org.hamcrest.core.Is;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TripServiceTest {
+	
+	private static final User GUEST = null;
+	private static final User UNUSED_USER = null;
+	private static final User REGISTERD_USER = new User();
+	private static final User ANOTHER_USER = new User();
+	private static final Trip TO_BRAZIL = new Trip();
+	private User loggedInUser;
+	
+	private TripService tripService;
+
+	@Before
+	public void initialise(){
+		tripService = new TestableTripService();
+		
+	}
+	
 	@Test(expected = UserNotLoggedInException.class) public void
 	should_throw_and_exception_when_is_not_logged_in(){
 		
-		TripService tripService = new TestableTripService();
+		loggedInUser = GUEST;
 		
-		tripService.getTripsByUser(null);
+		tripService.getTripsByUser(UNUSED_USER);
+	}
+	
+	@Test public void
+	should_not_return_any_trips_when_users_are_not_friends(){
+		
+		loggedInUser = REGISTERD_USER;
+		
+		User friend = new User();
+		friend.addFriend(ANOTHER_USER);
+		friend.addTrip(TO_BRAZIL);
+		
+		List<Trip> friendTrips = tripService.getTripsByUser(friend);
+		
+		assertThat(friendTrips.size(), org.hamcrest.Matchers.is(0));
+		
 	}
 	
 	private class TestableTripService extends TripService{
 
 		@Override
 		protected User getLoggedInUser() {
-			return null;
+			return loggedInUser ;
 		}
 		
-	}
+	} 
 }
