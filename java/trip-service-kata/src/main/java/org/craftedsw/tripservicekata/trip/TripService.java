f3127f53490361ae880ee2200cf7a28d84528df2
sereno125 <sereno125@gmail.com>
2017-07-02 15:30:18 +0900

    TripServcie refactored

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,30 +10,42 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
 		//loggedUser가 나. 내가user의 친구이면 목록 볼수있다. 
-		User loggedUser = getLoggedInUser();
-		boolean isFriend = false;
+		if(getLoggedInUser() == null){
+			throw new UserNotLoggedInException();
+		}
+		
+		
+		return user.isFriendsWith(getLoggedInUser())
+						? tripsBy(user)
+						: noTrips();
+			
 		//1. 제일 얕은 indent
 		//- 로그인되지 않은 유저이면 exception
-		if (loggedUser != null) {
+		//if (loggedInUser != null) {
 			// 두번째로 깊은 indent
 			// 코드스멜 중 feature envy
 			// user의 일을 tripservice가 탐내고 있다.user의 일은 user에게 물어봐야 한다.
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
+//			for (User friend : user.getFriends()) {
+//				if (friend.equals(loggedInUser)) {
+//					isFriend = true;
+//					break;
+//				}
+//			}
 			//가장 깊은 indent 
-			if (isFriend) {
-				tripList = tripsBy(user);
-			}
-			return tripList;
-		} else {
-			throw new UserNotLoggedInException();
-		}
+//			if (user.isFriendsWith(loggedInUser)) {
+//				tripList = tripsBy(user);
+//			}
+//			return tripList;
+		//}
+		// 이부분은 guard clause로 바꿀수 있다  
+//		else {
+//			throw new UserNotLoggedInException();
+//		}
+	}
+
+	private ArrayList<Trip> noTrips() {
+		return new ArrayList<Trip>();
 	}
 
 	protected List<Trip> tripsBy(User user) {
